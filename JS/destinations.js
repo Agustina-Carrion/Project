a3f456f1039b605bb4a71f5d99c3c52c07b90036
Agustina Carrion <agus.carrion@icloud.com>
2022-10-08 12:27:18 +0200

    updated destinations page

--- JS/destinations.js
+++ JS/destinations.js
@@ -2,7 +2,6 @@ function createCard(card, index) {
   const cardTemplate = `
   <div class="column is-one-third-desktop is-flex">
   <div class="card">
-  <div id="weatherBox-${card.city}">Weather's loading</div>
   <button data-index="${index}" class="delete is-large"></button>
     <div class="card-image">
       <figure class="image">
@@ -13,7 +12,7 @@ function createCard(card, index) {
       </figure>
     </div>
     <div class="card-content">
-      <div class="media">
+      <div class="media is-align-items-center">
         <div class="media-left">
           <figure class="image is-48x48">
             <img id="weatherIcon-${card.city}"
@@ -21,6 +20,7 @@ function createCard(card, index) {
               alt="${card.thumbnailAltText}"
             />
           </figure>
+        <div id="weatherBox-${card.city}" style="width: 150px">Weather's loading</div>
         </div>
         <div class="media-content">
           <p class="title is-4">${card.name}</p>
@@ -49,7 +49,6 @@ function renderCards() {
 }
 
 const cardContainer = document.querySelector("#cardContainer");
-const weatherBox = document.querySelector("#weatherBox");
 const weatherIcon = document.querySelector(".weatherIcon");
 
 fetch("./destinations.json")
@@ -76,29 +75,12 @@ function fetchWeather(city) {
       const weatherIcon = document.querySelector(`#weatherIcon-${city}`);
       weatherIcon.src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
       const weatherBox = document.querySelector(`#weatherBox-${city}`);
-      weatherBox.innerHTML = `In ${result.name}, the weather is ${result.weather[0].main}, temp ${result.main.temp}`;
+      weatherBox.innerHTML = `<strong>${result.name}</strong>,<em> ${result.weather[0].main}</em> <br/> Temp ${result.main.temp}°`;
     })
     .catch(function (error) {
       console.log(error);
-      // alert("There's an error somewhere!");
     });
 }
-
-// weatherBox.addEventListener("click", function () {
-//   alert("yay!");
-//   fetch("https://openweathermap.org/img/wn/04d@4x.png")
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (result) {
-//       console.log(result);
-//       const icon = result.weather[0].icon;
-//       weatherIcon.src = icon;
-//     })
-//     .catch(function (error) {
-//       alert("There's an error somewhere!");
-//     });
-// });
 
 cardContainer.addEventListener("click", function (event) {
   if (event.target.tagName !== "BUTTON") {
